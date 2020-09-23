7b556b43ccbf7acc9a4756b774397aede1eb7dc9
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-09-23 18:19:45 +0000

    stdin to make sure we exit game okay

--- play.js
+++ play.js
@@ -5,3 +5,26 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 //calling the connect function from client file
 connect();
+
+// handle user input via stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+
+  const handleUserInput = function() {
+    stdin.on('data', (key) =>{
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+}
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  handleUserInput();
+  return stdin;
+}
+
+
+
+setupInput();
