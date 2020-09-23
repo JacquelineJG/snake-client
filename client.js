9314898f5ebf5416c3ff2795a0ef3a97128a8bd9
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-09-23 17:28:40 +0000

    client side sends name to game board to identify snake

--- client.js
+++ client.js
@@ -11,9 +11,16 @@ const connect = function () {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
-
+  // event handler for when snake idles for too long to print message
   conn.on('data', (data) => {
     console.log(data);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+  conn.on('connect', () => {
+    conn.write('Name: JJG');
   });
 
   return conn;
