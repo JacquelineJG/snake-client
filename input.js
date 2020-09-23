a9e8c1cde2cd1a6fbf5233410905b294e1347aa6
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-09-23 19:06:34 +0000

    made WASD movement keys take user input

--- input.js
+++ input.js
@@ -1,12 +1,31 @@
+let connection;
+
 // handle user input via stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
 
   const handleUserInput = function() {
     // grabs data and exits game upon getting command (control c)
-    stdin.on('data', (key) =>{
+    stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
+    } else if (key === '\u0057' || key === '\u0077') {
+      
+          conn.write('Move: up');
+      
+    } else if (key === '\u0041' || key === '\u0061') {
+      //conn.on('connect', () => {
+        conn.write('Move: left');
+      //});
+    } else if (key === '\u0053' || key === '\u0073') {
+      //conn.on('connect', () => {
+        conn.write('Move: down');
+     // });
+    } else if (key === '\u0044' || key === '\u0064') {
+      //conn.on('connect', () => {
+        conn.write('Move: right');
+      //});
     }
   })
 }
--- play.js
+++ play.js
@@ -4,6 +4,6 @@ const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 //calling function from client file
-connect();
 //calling function from input file
-setupInput();
+setupInput(connect());
+
