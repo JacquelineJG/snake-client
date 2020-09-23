da1b8e2681a89c935e096273acf8a7b5fb899eae
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-09-23 19:12:55 +0000

    added keys that send messages in game

--- input.js
+++ input.js
@@ -11,21 +11,27 @@ const setupInput = function(conn) {
     if (key === '\u0003') {
       process.exit();
     } else if (key === '\u0057' || key === '\u0077') {
-      
+      //w key
           conn.write('Move: up');
-      
     } else if (key === '\u0041' || key === '\u0061') {
-      //conn.on('connect', () => {
+      //a key
         conn.write('Move: left');
       //});
     } else if (key === '\u0053' || key === '\u0073') {
-      //conn.on('connect', () => {
+      //s key
         conn.write('Move: down');
-     // });
     } else if (key === '\u0044' || key === '\u0064') {
-      //conn.on('connect', () => {
+      //d key
         conn.write('Move: right');
-      //});
+    } else if (key === '\u0046' || key === '\u0066') {
+      //f key
+        conn.write('Say: hi friends');
+    } else if (key === '\u0045' || key === '\u0065') {
+      //e key
+        conn.write('Say: sssssss');
+    } else if (key === '\u0051' || key === '\u0071') {
+      //q key
+        conn.write('Say: weeeeee');
     }
   })
 }
